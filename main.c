29fa54cfa2ec502d3ed7b79c4cd2ceffd7befe7f
V <v@Vs-MacBook-Pro.local>
2018-09-27 15:18:41 +0700

    Lam viec voi file

--- main.c
+++ main.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
 #include <string.h>
-// fgets(3 tham so) stdin cong nhap vao true la so 1,fall la so 0.fgetc lay ra mot tu trong stdin
-// strchr tim ki tự trong chuỗi ng nhập ban đầu.fgets:  nơi sữ liệu đc đưa vào, nơi dữ liệu đc đưa
-void noiHoVaTen(char ho[], char ten[]) {
-    printf("Độ dài của họ là: %d\n", strlen(ho));
-    printf("Độ dài của tên là: %d\n", strlen(ten));
-    int doDaiHo = strlen(ho);
-    int doDaiTen = strlen(ten);
-    int tong = doDaiHo + doDaiTen;
-    printf("Độ dài của cả họ và tên là: %d\n", tong);
-    tong++;
-    char hovaten[tong];
-    strcpy(hovaten, ho);
-    strcat(hovaten, " ");
-    strcat(hovaten, ten);
-    printf("%s", hovaten);
-
-}
+#include <stdlib.h>
 int main() {
-        char ho[10], ten[10];
-    printf("Vui lòng nhập họ của bạn:\n");
-    fgets(ho, 10, stdin); // fgets thay thế cho get.
-    puts (ho);
-    if (!strchr(ho,'\n')) {
-        while (fgetc(stdin) != '\n');
+    // ghi file.
+    FILE *fp = fopen("hello_c.txt", "w+");
+    char choice;
+    char userInput[255];
+    printf("Enter something to save file:\n"); // Yêu cầu nhập.
+    while (1 == 1) {
+        fgets(userInput, 255, stdin);
+        //"exit".
+        if (strcmp(userInput, "exit\n") == 0) {
+            break;
+        }
+        fprintf(fp, userInput);
     }
-    printf("Vui lòng nhập tên của bạn:\n");
-    fgets(ten, 10, stdin);
-    puts (ten);
-    noiHoVaTen(ho, ten);
+    fclose(fp);
+    // đọc file.
+    printf("---------Read file data----------\n");
+    fp = fopen("hello_c.txt", "r");
+    int bufferSize = 255;
+    char buffer[bufferSize];
+    while (fgets(buffer, bufferSize, fp) != NULL) {
+        printf("%s", buffer);
+    }
     return 0;
 }
